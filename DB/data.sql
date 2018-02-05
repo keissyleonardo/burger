aa6e6876b1376a5efa16c19d9a7ea380e68494d2
Keissy <keissy@lifecellskincare.com>
2018-02-05 12:14:49 -0500

    started

---
+++ DB/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ('jamon', FALSE)
+INSERT INTO burgers(burger_name, devoured) VALUES ('queso', FALSE)
+INSERT INTO burgers(burger_name, devoured) VALUES ('Nada', FALSE)
